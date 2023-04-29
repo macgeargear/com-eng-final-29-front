4faa849c9b0e2e3d6828e4d52d689559bf2069ba
Gear <84141000+macgeargear@users.noreply.github.com>
2023-04-29 12:13:19 +0700

    modify-modal

--- script_modal.js
+++ script_modal.js
@@ -6,23 +6,23 @@ var closeBtn = modal.querySelector(".close");
 
 // When the user clicks on the button, open the modal
 function openModal(modalId) {
-    let thisModal = document.getElementById(modalId);
-    console.log(thisModal);
-    thisModal.style.display = "block";
+  let thisModal = document.getElementById(modalId);
+  console.log(thisModal);
+  thisModal.style.display = "block";
 }
 
 // When the user clicks on the close button or outside the modal, close the modal
 function closeModal(modalId) {
-    let thisModal = document.getElementById(modalId);
-    console.log(thisModal);
-    thisModal.style.display = "none";
+  let thisModal = document.getElementById(modalId);
+  console.log(thisModal);
+  thisModal.style.display = "none";
 }
 // closeBtn.onclick = function() {
 //   modal.style.display = "none";
 // }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
