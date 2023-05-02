ad5b3dbf99566018dcb997ddac9cfc2c2ff150f9
Gear <84141000+macgeargear@users.noreply.github.com>
2023-05-02 12:57:06 +0700

    modify IPAddress

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = "44.195.103.88:3000";
 
 const authorizeApplication = () => {
   window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
